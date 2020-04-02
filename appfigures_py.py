6703226694d83353b02d32e9e614f90c8bc1fb13
maxomator13 <max@tmation.com>
2020-04-02 15:20:07 +0200

    removed network filter

--- appfigures_py.py
+++ appfigures_py.py
@@ -60,12 +60,11 @@ class AppFigures(object):
         resp = self.session.get(url=self.BASE_URL + '/reports/sales', params=params)
         return resp.json()
 
-    def get_usage(self, start_date, end_date, group_by='network', networks='google_analytics'):
+    def get_usage(self, start_date, end_date, group_by='network'):
         params = {
             'start_date': start_date,
             'end_date': end_date,
             'group_by': group_by,
-            'networks': networks
         }
 
         resp = self.session.get(url=self.BASE_URL + '/reports/usage', params=params)
